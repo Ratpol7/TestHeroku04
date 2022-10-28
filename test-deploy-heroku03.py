c9218df0418ac7a9d779cddfb26aa0ab0fb54f7c
Ratpol <patnan23@gmail.com>
2022-10-28 13:47:42 +0700

    Update test-deploy-heroku03.py

--- test-deploy-heroku03.py
+++ test-deploy-heroku03.py
@@ -2,7 +2,6 @@ from line_notify import LineNotify
 import os
 Line_Notify = "RDUnfnGrcnb81X9wpZUjWGf8GdtHNgCkMP76i9ANCKj"
 notify = LineNotify(Line_Notify)
-t = 1
-if t == 1:
-    notify.send("TestDeploy", sticker_id=17851, package_id=1070)
-    t = 0
+
+notify.send("TestDeploy", sticker_id=17851, package_id=1070)
+
