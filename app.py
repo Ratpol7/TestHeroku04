9024dfe2b04deb38493215c7c444aff8ed17e2ce
Ratpol <patnan23@gmail.com>
2022-10-28 18:37:33 +0700

    1

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from line_notify import LineNotify
+from flask import Flask
 Line_Notify = "RDUnfnGrcnb81X9wpZUjWGf8GdtHNgCkMP76i9ANCKj"
 notify = LineNotify(Line_Notify)
-print("/n/ntesttttttt/n/n")
-notify.send("TestDeploy", sticker_id=17851, package_id=1070)
+
+app = Flask(__name__)
+
+if __name__ == "__main__":
+    print("/n/ntesttttttt/n/n")
+    notify.send("TestDeploy", sticker_id=17851, package_id=1070)
 
