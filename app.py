ca9b255f588dea79bca6ad281151fbef1d2ab7e1
Ratpol <patnan23@gmail.com>
2022-11-05 20:17:31 +0700

    Update app.py

--- app.py
+++ app.py
@@ -13,11 +13,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_view():
-    notify.send("TestDeploy", sticker_id=17851, package_id=1070)
-    return "<h1>Hello World!<h1>"+df.to_html(header="true", table_id="table")
-# @app.route('/a')
-# def pandas():
-    # return df.to_html(header="true", table_id="table")
+    # notify.send("TestDeploy", sticker_id=17851, package_id=1070)
+    return "<h1>Hello World!<h1>"
+@app.route('/pandas')
+def pandas():
+    return df.to_html(header="true", table_id="table")
 if __name__ == "__main__":
     # app.run(host='0.0.0.0',port=80,debug=True)
     app.run()
