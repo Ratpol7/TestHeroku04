12c25a8257692f486ff385e463e1f80402b31c6f
Ratpol <patnan23@gmail.com>
2022-11-05 20:06:11 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 from line_notify import LineNotify
 from flask import Flask, jsonify
-# LINE_TOKEN=str(os.environ['Line_Notify_Token'])
-Line_Notify = "RDUnfnGrcnb81X9wpZUjWGf8GdtHNgCkMP76i9ANCKj"
+import pandas as pd
+
+df = pd.DataFrame({'col1': ['abc', 'def', 'tre'],
+                   'col2': ['foo', 'bar', 'stuff']})
+
+Line_Notify = str(os.environ['Line_Notify_Token'])
+# Line_Notify = "RDUnfnGrcnb81X9wpZUjWGf8GdtHNgCkMP76i9ANCKj"
 notify = LineNotify(Line_Notify)
 
 app = Flask(__name__)
@@ -9,8 +14,9 @@ app = Flask(__name__)
 @app.route('/')
 def hello_view():
     # notify.send("TestDeploy", sticker_id=17851, package_id=1070)
-    
-    return "<h1>Hello World!<h1>"+"55555"
+    return "<h1>Hello World!<h1>"
+def pandas():
+    return df.to_html(header="true", table_id="table")
 if __name__ == "__main__":
     # app.run(host='0.0.0.0',port=80,debug=True)
     app.run()
