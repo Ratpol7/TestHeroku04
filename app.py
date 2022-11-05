d01cfaebd0c2d55d617b58dafec51a5dc0c055d6
Ratpol <patnan23@gmail.com>
2022-11-05 19:28:38 +0700

    v2

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
 from line_notify import LineNotify
-from flask import Flask
+from flask import Flask, jsonify
 # LINE_TOKEN=str(os.environ['Line_Notify_Token'])
 Line_Notify = "RDUnfnGrcnb81X9wpZUjWGf8GdtHNgCkMP76i9ANCKj"
 notify = LineNotify(Line_Notify)
 
 app = Flask(__name__)
 
-app.callback
-if __name__ == "__main__":
-    print("/n/ntesttttttt/n/n")
+app.route('/')
+def hello_line():
     notify.send("TestDeploy", sticker_id=17851, package_id=1070)
+    return "Hello"
+if __name__ == "__main__":
+    while True:
+        app.run(host='0.0.0.0',port=80)
 
