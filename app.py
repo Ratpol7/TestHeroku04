8b5fe3493fa11a368ef1f750467d02cc0698c98b
Ratpol <patnan23@gmail.com>
2022-11-05 19:52:10 +0700

    Update app.py

--- app.py
+++ app.py
@@ -11,5 +11,6 @@ def hello_view():
     # notify.send("TestDeploy", sticker_id=17851, package_id=1070)
     return "<h1>Hello World!<h1>"
 if __name__ == "__main__":
-    app.run(host='0.0.0.0',port=80,debug=True)
+    # app.run(host='0.0.0.0',port=80,debug=True)
+    app.run()
 
