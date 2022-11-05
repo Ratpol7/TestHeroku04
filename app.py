4fcd9b632a9c8b13509807b07cadb349fb71fb0c
Ratpol <patnan23@gmail.com>
2022-11-05 20:13:35 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5,15 +5,15 @@ import pandas as pd
 df = pd.DataFrame({'col1': ['abc', 'def', 'tre'],
                    'col2': ['foo', 'bar', 'stuff']})
 
-Line_Notify = str(os.environ['Line_Notify_Token'])
-# Line_Notify = "RDUnfnGrcnb81X9wpZUjWGf8GdtHNgCkMP76i9ANCKj"
+# Line_Notify = str(os.environ['Line_Notify_Token'])
+Line_Notify = "RDUnfnGrcnb81X9wpZUjWGf8GdtHNgCkMP76i9ANCKj"
 notify = LineNotify(Line_Notify)
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_view():
-    # notify.send("TestDeploy", sticker_id=17851, package_id=1070)
+    notify.send("TestDeploy", sticker_id=17851, package_id=1070)
     return "<h1>Hello World!<h1>"+df.to_html(header="true", table_id="table")
 # @app.route('/a')
 # def pandas():
