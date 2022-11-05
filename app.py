88345e10dc79d116fbe40c33ebfe9df598310340
Ratpol <patnan23@gmail.com>
2022-11-06 01:13:48 +0700

    v7

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ from line_notify import LineNotify
 from flask import Flask, jsonify
 import pandas as pd
 import os
+import mplfinance as mpf
+import matplotlib.pyplot as plt
+import yfinance as yf
 df = pd.DataFrame({'col1': ['abc', 'def', 'tre'],
                    'col2': ['foo', 'bar', 'stuff']})
 
@@ -15,9 +18,19 @@ app = Flask(__name__)
 def hello_view():
     notify.send("TestDeploy", sticker_id=17851, package_id=1070)
     return "<h1>Hello World!<h1>"
+
 @app.route('/pandas')
 def pandas():
     return df.to_html(header="true", table_id="table")
+
+@app.route('/yfinance')
+def yfinance():
+    yahoo_financials = YahooFinancials('BTC-USD')
+    data=yahoo_financials.get_historical_price_data("2019-07-10", "2021-05-30", "monthly")
+    btc_df = pd.DataFrame(data['BTC-USD']['prices'])
+    btc_df = btc_df.drop('date', axis=1).set_index('formatted_date')
+    btc_df.head()
+    return data
 if __name__ == "__main__":
     # app.run(host='0.0.0.0',port=80,debug=True)
     app.run()
