2c4540c8d7d0c50b99abdf68dc00ae97c588cc75
Ratpol <patnan23@gmail.com>
2022-11-05 19:56:23 +0700

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_view():
-    notify.send("TestDeploy", sticker_id=17851, package_id=1070)
+    # notify.send("TestDeploy", sticker_id=17851, package_id=1070)
+    print("55555")
     return "<h1>Hello World!<h1>"
 if __name__ == "__main__":
     # app.run(host='0.0.0.0',port=80,debug=True)
