20394f9747e61da013f75ef3131086ddff633dff
Ratpol <patnan23@gmail.com>
2022-11-06 01:14:32 +0700

    Update app.py

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ def pandas():
 
 @app.route('/yfinance')
 def yfinance():
-    yahoo_financials = YahooFinancials('BTC-USD')
+    yahoo_financials = yf('BTC-USD')
     data=yahoo_financials.get_historical_price_data("2019-07-10", "2021-05-30", "monthly")
     btc_df = pd.DataFrame(data['BTC-USD']['prices'])
     btc_df = btc_df.drop('date', axis=1).set_index('formatted_date')
